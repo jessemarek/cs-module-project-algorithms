33cd92f35ac29943a081ca787a69947cd08857be
unknown <dethgrenade@gmail.com>
2020-07-22 12:23:51 -0700

    WiP day 1 - moving zeroes done

--- moving_zeroes/moving_zeroes.py
+++ moving_zeroes/moving_zeroes.py
@@ -2,10 +2,18 @@
 Input: a List of integers
 Returns: a List of integers
 '''
-def moving_zeroes(arr):
-    # Your code here
 
-    pass
+
+def moving_zeroes(arr):
+
+    # loop through each item in the list
+    for i in range(len(arr) - 1):
+        # check the next value and compare.
+        # If a 0 is on the left swap spots
+        for j in range(len(arr) - i - 1):
+            if arr[j] == 0 and arr[j+1] != 0:
+                arr[j], arr[j+1] = arr[j+1], arr[j]
+    return arr
 
 
 if __name__ == '__main__':
