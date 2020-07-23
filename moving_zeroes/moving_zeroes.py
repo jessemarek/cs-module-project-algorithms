c545b03249e05885b4a8cc712bcbd43049fa4110
unknown <dethgrenade@gmail.com>
2020-07-23 12:12:21 -0700

    WiP day 2 - moving zeroes works in a single pass

--- moving_zeroes/moving_zeroes.py
+++ moving_zeroes/moving_zeroes.py
@@ -5,14 +5,23 @@ Returns: a List of integers
 
 
 def moving_zeroes(arr):
-
-    # loop through each item in the list
+    """ # loop through each item in the list
     for i in range(len(arr) - 1):
         # check the next value and compare.
         # If a 0 is on the left swap spots
         for j in range(len(arr) - i - 1):
             if arr[j] == 0 and arr[j+1] != 0:
                 arr[j], arr[j+1] = arr[j+1], arr[j]
+    return arr """
+
+    # look at each item in list
+    for i in range(len(arr)):
+        # if the index value is 0, remove it from
+        # the list and add a 0 to the end
+        if arr[i] == 0:
+            arr.pop(i)
+            arr.append(0)
+
     return arr
 
 
